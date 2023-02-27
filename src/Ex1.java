03b94a5a3d3e2074aa440044e075876b1679f0cc
SerenaCode <serenawork2103@gmail.com>
2023-02-27 13:08:15 +0700

    Initial commit

---
+++ src/Ex1.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        // Expression (a)
+        int resultA = (101 + 0) / 3;
+        System.out.println("(101 + 0) / 3 -> " + resultA);
+
+        // Expression (b)
+        double resultB = 3.0e-6 * 10000000.1;
+        System.out.println("3.0e-6 * 10000000.1 -> " + resultB);
+
+        // Expression (c)
+        boolean resultC = true && true;
+        System.out.println("true && true -> " + resultC);
+
+        // Expression (d)
+        boolean resultD = false && true;
+        System.out.println("false && true -> " + resultD);
+
+        // Expression (e)
+        boolean resultE = (false && false) || (true && true);
+        System.out.println("(false && false) || (true && true) -> " + resultE);
+
+        // Expression (f)
+        boolean resultF = (false || false) && (true && true);
+        System.out.println("(false || false) && (true && true) -> " + resultF);
+    }
+}
